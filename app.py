98fd75ab69178e412c6dd657da7920a8e623ad22
mattyturn95 <46969824+mattyturn95@users.noreply.github.com>
2020-02-08 14:46:20 +0000

    mongo added to app .py

--- app.py
+++ app.py
@@ -13,3 +13,24 @@ from flask import (
     session,
     url_for,
 )
+
+from flask_login import LoginManager
+from utilities import paginate
+from validators import validate_recipe
+
+load_dotenv()
+
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+# Get secret key
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+login = LoginManager(app)
+
+# Mongo Database for FITNESS POT
+myclient = pymongo.MongoClient(os.environ.get("MONGO_CLUSTER"))
+mydb = myclient["fitness_pot"]
+dish_col = mydb["dish"]
+users_col = mydb["users"]
+collection_names = mydb.list_collection_names()
